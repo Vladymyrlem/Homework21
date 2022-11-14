7d7e103c894ade4d460d7b0e556dcd9e8a86a475
Vladymyr <vovangud@gmail.com>
2022-11-14 21:18:12 +0200

    Creating Providers

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Label;
+use App\Models\Project;
+use App\Policies\LabelPolicy;
+use App\Policies\ProjectPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        Project::class => ProjectPolicy::class,
+        Label::class   => LabelPolicy::class
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
