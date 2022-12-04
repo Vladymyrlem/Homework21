d7abd2be0674c01eee3614392deb44138a522feb
Vladymyr <vovangud@gmail.com>
2022-12-04 19:24:46 +0200

    Creating Task 2

--- public/dz-file.php
+++ public/dz-file.php
@@ -49,3 +49,55 @@ try {
     echo $exception->getMessage();
 }
 
+//2
+//2.1 створюємо таблицю logs (id, entity_id, entity_name, action, content)
+$query = 'CREATE TABLE logs (
+    id int primary key auto_increment,
+    `action` ENUM("create", "update", "delete") not null,
+    entity_id int not null ,
+    entity_name  VARCHAR(255) not null ,
+    content json not null )';
+$mysql->query($query);
+
+//2.2 створюємо трігер, який буде спрацьовувати після insert (тут записуємо інформацію в таблицю logs)
+$query = "CREATE TRIGGER `insert_projects`
+    AFTER INSERT ON projects
+    FOR EACH ROW BEGIN
+    INSERT INTO logs (`action`, entity_id, entity_name, content)
+    VALUES ('insert', NEW.id, 'project',
+            JSON_OBJECT('id', NEW.id, 'name', NEW.name, 'author_id', NEW.author_id, 'created_at', NEW.created_at, 'updated_at', NEW.updated_at));
+END;";
+$mysql->query($query);
+
+
+//2.3 створюємо трігер, який буде спрацьовувати перед оновленням (тут записуємо інформацію в таблицю logs)
+$query = "CREATE TRIGGER `update_projects`
+    BEFORE UPDATE
+    ON projects
+    FOR EACH ROW
+BEGIN
+    INSERT INTO logs (`action`, entity_id, entity_name, content)
+    VALUES ('update', OLD.id, 'project',
+            JSON_OBJECT('id', OLD.id, 'name', OLD.name, 'author_id', OLD.author_id, 'created_at', OLD.created_at, 'updated_at', OLD.updated_at));
+END;";
+$mysql->query($query);
+
+
+//2.4 створюємо трігер, який буде спрацьовувати перед видаленням (тут записуємо інформацію в таблицю logs)
+$query = "CREATE TRIGGER `delete_projects`
+    BEFORE DELETE
+    ON projects
+    FOR EACH ROW
+BEGIN
+    INSERT INTO logs (`action`, entity_id, entity_name, content)
+    VALUES ('delete', OLD.id, 'project',
+            JSON_OBJECT('id', OLD.id, 'name', OLD.name, 'author_id', OLD.author_id, 'created_at', OLD.created_at, 'updated_at', OLD.updated_at));
+END;";
+$mysql->query($query);
+
+
+//Після цього написати sql запити, на створення нового запису в project
+
+//UPdate
+
+//Delete
